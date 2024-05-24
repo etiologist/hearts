140cff7d506ab3246d607c5d3e24b985b16b2f23
etiologist <65084030+etiologist@users.noreply.github.com>
2024-05-24 14:46:05 +0100

    ss

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,15 +1,14 @@
 const button = document.getElementById("loading-button");
 const square = document.getElementById("square");
-const empty =  document.getElementById("empty");
 
 button.addEventListener("click", () => {
   // Disable the button to prevent multiple clicks
   button.disabled = true;
 
   // Add the loading animation CSS class
-  //button.classList.add("button-loader");
+  
   square.classList.add("box1");
-  //empty.classList.add("box2");
+  
 
   // Simulate an asynchronous task (replace this with your actual task)
   setTimeout(() => {
@@ -18,7 +17,7 @@ button.addEventListener("click", () => {
 
     // Remove the loading animation CSS class
     square.classList.remove("box1");
-    //empty.classList.remove("box2");
+    
 
   }, 2000); // Simulated 2-second task
 });
