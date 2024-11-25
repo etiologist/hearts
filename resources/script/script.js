99d9253bd0c220256d8688bae9d66e1f857b27e0
etiologist <65084030+etiologist@users.noreply.github.com>
2024-11-25 20:12:21 +0000

    tt

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,50 +1,47 @@
 const heartsBox = document.querySelector('.hearts');
 const hearts = Array.from(document.querySelectorAll('.heart'));
 const button = document.querySelector('.button');
-const { innerWidth, innerHeight } = window;
+const {
+  innerWidth,
+  innerHeight
+} = window;
 let currentAnimation;
-
-gsap.set(heartsBox, { visibility: 'visible' });
-gsap.set(hearts, { opacity: 0 });
-
-const heartPop = ({ x, y }) => {
+gsap.set(heartsBox, {
+  visibility: 'visible'
+});
+gsap.set(hearts, {
+  opacity: 0
+});
+const heartPop = ({
+  x,
+  y
+}) => {
   if (currentAnimation) currentAnimation.kill();
-
   currentAnimation = gsap.timeline().addLabel('start');
-
   hearts.forEach((heart, index) => {
-    const heartAnimation = gsap.
-    timeline().
-    set(heart, {
+    const heartAnimation = gsap.timeline().set(heart, {
       opacity: 1,
       scale: 0,
       x: x,
-      y: y }).
-
-    to(
-    heart,
-    {
+      y: y
+    }).to(heart, {
       duration: 0.5,
       ease: 'power1.out',
       rotate: 60 - Math.random() * 120,
       scale: 0.2 + Math.random() * 1,
       delay: index * 0.02,
       x: x + innerWidth / 4 - Math.random() * innerWidth / 2,
-      y: y - 50 - Math.random() * (innerHeight / 4) }).
-
-
-    to(heart, {
+      y: y - 50 - Math.random() * (innerHeight / 4)
+    }).to(heart, {
       duration: 0.5,
       opacity: 0,
       y: y + 200,
       rotate: 0,
-      ease: 'power1.in' });
-
-
+      ease: 'power1.in'
+    });
     currentAnimation.add(heartAnimation, 'start');
   });
 };
-
 button.addEventListener('click', event => {
   heartPop(event);
 });
